e45dcdea42ea35f19cc6872be208784e6ae41e0e
Deleon Francis <dxfrancis.coding@gmail.com>
2021-07-27 09:43:14 -0400

    conditionally rendering color of jumboWeather with modal.

--- client/src/components/guestUser/weather/JumboWeather.js
+++ client/src/components/guestUser/weather/JumboWeather.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+
+let selectedTheme = "";
 
 const useStyles = makeStyles((theme) => ({
-    
-    weatherDetail: {
-      flexGrow: 1,
-      alignSelf: "flex-center",
-      marginTop: "20px"
-      // fontSize: "3.0vw",
-    },
-  }));
-
+  weatherDetail: {
+    flexGrow: 1,
+    alignSelf: "flex-center",
+    marginTop: "20px",
+  },
+  text: {
+    color: selectedTheme === "dark" ? "black" : "white",
+  },
+}));
 
 function JumboWeather() {
-    const classes = useStyles();
-    return (
-        <button className={classes.weatherDetail} align="center">
-            this is where the current weather will go.
-        </button>
-    )
+  const classes = useStyles();
+  const userTheme = useSelector((state) => state.guestThemeReducer);
+  selectedTheme = `${userTheme.userTheme}`;
+  console.log(selectedTheme);
+  return (
+    <div className={classes.weatherDetail}>
+      <Link
+        component="button"
+        variant="body2"
+        underline="none"
+      >
+        <div className={classes.text}>
+          this is where the current weather will go.
+        </div>
+      </Link>
+    </div>
+  );
 }
 
-export default JumboWeather
+export default JumboWeather;
