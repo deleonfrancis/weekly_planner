5a49fff8f3a9101c9c359bf20c805c43937dc736
Deleon Francis <dxfrancis.coding@gmail.com>
2021-07-27 16:35:06 -0400

    conditionaly rendering h3 for weather modal.

--- client/src/components/guestUser/weather/WeatherModalPage.js
+++ client/src/components/guestUser/weather/WeatherModalPage.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+// import { useSelector, useDispatch } from "react-redux";
 
 function WeatherModalPage() {
-    return (
-        <div>
-            <div>This is where all current weather data will go</div>
-        </div>
-    )
+  const { currentWeather } = useSelector((state) => state.weatherReducer);
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    console.log(currentWeather);
+  }, [currentWeather]);
+
+  return (
+    <div>
+      {currentWeather.location.country === "United States of America" ? (
+        <h3>
+          {currentWeather.location.name}, {currentWeather.location.region}
+        </h3>
+      ) : (
+        <h3>
+          {currentWeather.location.name}, {currentWeather.location.country}
+        </h3>
+      )}
+    </div>
+  );
 }
 
-export default WeatherModalPage
+export default WeatherModalPage;
