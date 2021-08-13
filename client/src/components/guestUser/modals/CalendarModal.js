a5b3b844fff01e63fed742cc92ac7405a398340b
Deleon Francis <dxfrancis.coding@gmail.com>
2021-08-13 09:12:48 -0400

    calendar in CalendarModal is now showing options for month, week, day, and list.

--- client/src/components/guestUser/modals/CalendarModal.js
+++ client/src/components/guestUser/modals/CalendarModal.js
@@ -1,14 +1,26 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
-// import interactionPlugin from "@fullcalendar/interaction"; // a plugin!
+import interactionPlugin from "@fullcalendar/interaction"; // a plugin!
+// import { Calendar } from "@fullcalendar/core";
+// import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 function CalendarModal() {
+  // let calendar = new Calendar(calendarEl, {
+  //   plugins: [timeGridPlugin],
+  //   initialView: "timeGridWeek",
+  // });
   return (
-    <div style={{width:"800px", height:"700px"}}>
+    <div style={{ width: "800px", height: "700px" }}>
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
         initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,dayGridWeek,dayGridDay,listWeek",
+        }}
       />
     </div>
   );
