31951de9f9a534e41fd7f71c2dca8d0fa5993e2a
Deleon Francis <dxfrancis.coding@gmail.com>
2021-08-13 10:35:53 -0400

    time now showing in week and day of calendar.

--- client/src/components/guestUser/modals/CalendarModal.js
+++ client/src/components/guestUser/modals/CalendarModal.js
@@ -2,24 +2,20 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin from "@fullcalendar/interaction"; // a plugin!
-// import { Calendar } from "@fullcalendar/core";
-// import timeGridPlugin from "@fullcalendar/timegrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 
 function CalendarModal() {
-  // let calendar = new Calendar(calendarEl, {
-  //   plugins: [timeGridPlugin],
-  //   initialView: "timeGridWeek",
-  // });
+
   return (
     <div style={{ width: "800px", height: "700px" }}>
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin,timeGridPlugin]}
         initialView="dayGridMonth"
         headerToolbar={{
           left: "prev,next today",
           center: "title",
-          right: "dayGridMonth,dayGridWeek,dayGridDay,listWeek",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         }}
       />
     </div>
